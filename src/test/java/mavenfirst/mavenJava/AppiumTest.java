8d3c3ca05dc4b5bc39ac9fc1a78dd73e6b83fae8
ravulavpk <ravula.vpk@gmail.com>
2022-01-23 16:04:37 +0000

    first commit

---
+++ src/test/java/mavenfirst/mavenJava/AppiumTest.java
@@ -0,0 +1,13 @@
+package mavenfirst.mavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+@Test
+
+public void test1() 
+{
+	System.out.println("Appium test output");
+}
+}
---
+++ src/test/java/mavenfirst/mavenJava/SeleniumTest.java
@@ -0,0 +1,14 @@
+package mavenfirst.mavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+@Test
+
+public void test1() 
+{
+	System.out.println("Selenium test output");
+}
+
+}
